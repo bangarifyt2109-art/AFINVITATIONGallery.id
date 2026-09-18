6849c3bf104f9b99c51b185c12a9ca4fb319816d
bangarifyt2109-art <bangarifyt2109@gmail.com>
2026-09-18 21:00:31 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -65,7 +65,7 @@ fetch("data/katalog.json")
   });
 function tampilkan(){
   const katalog = document.getElementById("katalog");
-  const nomorWA = "628";
+  const nomorWA = "6288801891778";
 
   katalog.innerHTML = "";
 
@@ -104,7 +104,7 @@ Saya mau pesan undangan
 
 <!-- harga -->
 <div class="text-center text-[#ee4d2d] text-base font-bold mt-1 mb-3">
-  Rpxxx - Rpxxx
+  Rp65.000 - Rp100.000
 </div>
 
 <!-- tombol -->
